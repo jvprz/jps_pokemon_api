4487b1fe6d7977988b079f85204ebbfeda429eae
Javier Prz <javierpsantos99@gmail.com>
2024-02-22 17:16:42 +0100

    getRandomPokemon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,20 +8,28 @@ import { PokeApi } from './pokeapi';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'jps_pokemon_api';
   pokemon!: PokeApi;
+  pokedex!: number;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.pokedex = this.getRandomNumber(1, 1025);
+  }
 
   ngOnInit() {
     this.getPokemon();
   }
 
   getPokemon() {
-    const url = 'https://pokeapi.co/api/v2/pokemon/7';
+    const url = 'https://pokeapi.co/api/v2/pokemon/' + this.pokedex;
     this.http.get<PokeApi>(url).subscribe((data: PokeApi) => {
       this.pokemon = data;
+      // Capitalize Pokemon name
+      this.pokemon.name = this.pokemon.name.charAt(0).toUpperCase() + this.pokemon.name.slice(1);
     });
+  }
+
+  getRandomNumber(min: number, max: number):number {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
 }
